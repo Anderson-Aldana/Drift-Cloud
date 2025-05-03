3a2d6cc2f70bc5f6bfa476cadffca86a2dd480c5
Anderson-Aldana <anderpach123@gmail.com>
2025-05-03 13:49:28 -0500

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function pedirProducto(nombre) {
-  const telefono = '51976404496'; // Reemplaza con el número del vendedor
-  const mensaje = `Hola, quiero pedir el producto: ${nombre}. ¿Está disponible?`;
-  const url = `https://wa.me/${telefono}?text=${encodeURIComponent(mensaje)}`;
-  window.open(url, '_blank');
-}
